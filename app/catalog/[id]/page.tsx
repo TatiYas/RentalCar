c27d0ca384698e86eff54b61a9ca3288e3a7a0e0
TatiYas <iasinovska_lawyer@ukr.net>
2026-03-15 22:48:03 +0100

    Create page.tsx

---
+++ app/catalog/[id]/page.tsx
@@ -0,0 +1,31 @@
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
+import { fetchCarById } from "../../../lib/api/serverApi";
+import CarDetailsClient from "./CarDetails.client";
+
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+
+const CarDetails = async ({ params }: Props) => {
+  const { id } = await params;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["car", id],
+    queryFn: () => fetchCarById(id),
+  });
+
+  return (
+      <HydrationBoundary state={dehydrate(queryClient)}>
+      <CarDetailsClient />
+    </HydrationBoundary>
+  );
+};
+
+export default CarDetails;
