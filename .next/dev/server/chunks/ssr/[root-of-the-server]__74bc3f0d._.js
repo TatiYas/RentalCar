ec140b474b8154505363a0c085da77b19a243872
TatiYas <iasinovska_lawyer@ukr.net>
2026-03-15 19:38:25 +0100

    U

--- .next/dev/server/chunks/ssr/[root-of-the-server]__74bc3f0d._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__74bc3f0d._.js
@@ -2945,7 +2945,14 @@ function getImageProps(imgProps) {
             "dangerouslyAllowSVG": ("TURBOPACK compile-time value", false),
             "unoptimized": ("TURBOPACK compile-time value", false),
             "domains": ("TURBOPACK compile-time value", []),
-            "remotePatterns": ("TURBOPACK compile-time value", []),
+            "remotePatterns": ("TURBOPACK compile-time value", [
+                ("TURBOPACK compile-time value", {
+                    "protocol": ("TURBOPACK compile-time value", "https"),
+                    "hostname": ("TURBOPACK compile-time value", "ac.goit.global"),
+                    "port": ("TURBOPACK compile-time value", ""),
+                    "pathname": ("TURBOPACK compile-time value", "/**")
+                })
+            ]),
             "localPatterns": ("TURBOPACK compile-time value", [
                 ("TURBOPACK compile-time value", {
                     "pathname": ("TURBOPACK compile-time value", "**"),
