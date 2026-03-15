1e9a409d9b4b05f6542160d151469f31417a2c2b
TatiYas <iasinovska_lawyer@ukr.net>
2026-03-15 20:03:25 +0100

    .

--- .next/dev/static/chunks/Desktop_Projects_RentalCar_app_layout_tsx_d20d3f47._.js
+++ .next/dev/static/chunks/Desktop_Projects_RentalCar_app_layout_tsx_d20d3f47._.js
@@ -1,7 +1,9 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[root-of-the-server]__41c900f1._.css"
+  "static/chunks/[root-of-the-server]__63fa344b._.css",
+  "static/chunks/6f1e8_38e86ac6._.js",
+  "static/chunks/3d860_Projects_RentalCar_components_TanStackProvider_TanStackProvider_tsx_c84ed5df._.js"
 ],
     source: "dynamic"
 });
