5723022e3ce431c1e613e00d561179be40233616
TatiYas <iasinovska_lawyer@ukr.net>
2026-03-15 22:18:26 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import path from 'path';
+
+export default {
  content: [
  './app/**/*.{js,ts,jsx,tsx,mdx}',
  './pages/**/*.{js,ts,jsx,tsx,mdx}',
  './components/**/*.{js,ts,jsx,tsx,mdx}',
  './src/**/*.{js,ts,jsx,tsx,mdx}',
- path.join(path.resolve(__dirname, './'), 'node_modules/.pnpm', '**/node_modules', '**/*.{js,ts,jsx,tsx,mdx}'),
+ path.join(path.resolve('./'), 'node_modules', '**/*.{js,ts,jsx,tsx,mdx}'),
  ],
  theme: {
  extend: {},
  },
  plugins: [],
-}
+};
 
 
 
